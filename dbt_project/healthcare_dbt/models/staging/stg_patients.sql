9d23e96bdb812d2763887c4ed129bb37d96ea059
Abhinav Anand <a8hinav.anand1508@gmail.com>
2026-07-07 13:11:12 +0530

    modified the stg_patient query

--- dbt_project/healthcare_dbt/models/staging/stg_patients.sql
+++ dbt_project/healthcare_dbt/models/staging/stg_patients.sql
@@ -1,10 +1,24 @@
-SELECT
-    patient_id,
-    first_name,
-    last_name,
-    gender,
-    date_of_birth,
-    city,
-    insurance_provider,
-    registration_date
-FROM {{ source('staging', 'patients') }}
+WITH source AS (
+    SELECT *
+    FROM {{ source('staging', 'patients') }}
+);
+
+cleaned AS (
+    SELECT
+        patient_id,
+        TRIM(first_name) AS first_name,
+        TRIM(last_name) AS last_name,
+        CONCAT(TRIM(first_name), ' ',TRIM(last_name)) AS patient_name,
+        LOWER(gender) AS gender,
+        CAST(date_of_birth AS DATE) AS date_of_birth,
+        CAST(registration_date AS DATE) AS registration_date,
+        INITCAP(city) AS city,
+        insurance_provider,
+        EXTRACT(YEAR FROM AGE(CURRENT_DATE, date_of_birth))::INT AS age
+    FROM source
+)
+
+
+SELECT *
+FROM cleaned
+
