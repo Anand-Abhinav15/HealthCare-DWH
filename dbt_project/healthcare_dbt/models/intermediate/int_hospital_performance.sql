94401b89cd3473f2002adfe0a5689086767b42a6
Abhinav Anand <a8hinav.anand1508@gmail.com>
2026-07-12 23:57:12 +0530

    added intermediate hospital_performance

---
+++ dbt_project/healthcare_dbt/models/intermediate/int_hospital_performance.sql
@@ -0,0 +1,23 @@
+WITH fact AS (
+    SELECT *
+    FROM {{ ref('fact_appointments') }}
+),
+
+hospitals AS (
+    SELECT *
+    FROM {{ ref('dim_hospitals') }}
+)
+
+SELECT
+    h.hospital_key,
+    h.hospital_name,
+    h.location,
+    h.hospital_type,
+    COUNT(f.appointment_id) AS total_appointments,
+    SUM(f.billing_amount) AS total_revenue,
+    AVG(f.treatment_cost) AS avg_treatment_cost,
+    AVG(f.appointment_duration) AS avg_appointment_duration
+FROM hospitals h
+LEFT JOIN fact f
+ON h.hospital_key = f.hospital_key
+GROUP BY h.hospital_key, h.hospital_name, h.location, h.hospital_type
