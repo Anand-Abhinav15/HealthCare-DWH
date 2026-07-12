bf57df0e4cf68295ec4d231d933a9385c4dac3fe
Abhinav Anand <a8hinav.anand1508@gmail.com>
2026-07-12 22:13:02 +0530

    added intermediate doctor_utilization

---
+++ dbt_project/healthcare_dbt/models/intermediate/int_doctor_utilization.sql
@@ -0,0 +1,20 @@
+WITH fact AS (
+    SELECT *
+    FROM {{ ref('fact_appointments') }}
+),
+
+doctor AS (
+    SELECT *
+    FROM {{ ref('dim_doctor') }}
+),
+
+SELECT
+    doctor.doctor_key,
+    doctor.doctor_name,
+    COUNT(*) AS appointments,
+    SUM(treatment_cost) AS revenue,
+    AVG(appointment_duration) AS avg_duration
+FROM fact
+JOIN doctor
+ON fact.doctor_key = doctor.doctor_key
+GROUP BY doctor.doctor_key, doctor.doctor_name
